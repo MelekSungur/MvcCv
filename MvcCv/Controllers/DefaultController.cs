2d71b0a0139975e8dc110c60dc1f37a11fee801e
melek <melek@MELEK>
2023-05-14 00:34:21 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities1 db = new DbCvEntities1();
+        public ActionResult Index()
+        {
+            var degerler=db.TblHakkimda.ToList();
+
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler=db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+      public PartialViewResult Egitim()
+        {
+            var egitim = db.TblEgitimlerim.ToList();
+            return PartialView(egitim);
+
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenek = db.TblYeteneklerim.ToList();
+            return PartialView(yetenek);
+
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobi = db.TblHobilerim.ToList();
+            return PartialView(hobi);
+
+        }
+    }
+}
